1f3793c95c6c74c8f4227708fc93b4413b46478d
tejashwinimanne <tejashwinimanne@gmail.com>
2026-04-03 17:09:30 +0530

    variable

---
+++ index.dart
@@ -0,0 +1,38 @@
+void main() {
+   print("Hello"); 
+
+  //Data Handling
+  //1.Variable - IS LIKE A BOX - It is helping us to store the data
+  //String - Anything in double quotes" or 'Text'
+    String name = "Teju";
+    print(name);
+   //int - Whole Number
+    int age = 19;
+    print(age);
+   //double - Decimal Number
+   double price = 99.99;
+   print(price);
+
+   //bool - true or false
+   bool isLoggedin = true;
+   print(isLoggedin);
+
+   // var keyword (Smart Type)
+   var username = "Teju";
+   var password = 12345;
+   var rating = 4.9;
+   
+   print(username);
+   print(password);
+
+   //dynamic (can change type)
+   dynamic data = "Teja";
+   data = 14;
+   data = true;
+   print(data);
+
+   //const - Fixed forever
+   const  appName = "MyApp";
+   print(appName);
+}
+
